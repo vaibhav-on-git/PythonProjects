2ded7ccef5e5a15fd879b71f2a910dce2cfe1b40
vaibhav <114700437+vaibhav-on-git@users.noreply.github.com>
2022-12-19 09:17:20 +0000

    check one note for changes

--- PyMotivatorBot/twilio_connect.py
+++ PyMotivatorBot/twilio_connect.py
@@ -1,7 +1,5 @@
 from twilio.rest import Client 
 #import os
-account_sid = 'AC464749a7c1346141b1c373b53c1785d1'
-auth_token = 'f6ad194dfb5833536a15f85000259a1f'
 #account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 #auth_token = os.environ.get('TWILIO_ACCOUNT_TOKEN')
 client = Client(account_sid, auth_token) 
